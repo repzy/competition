e712a019136356ef641f64c01a2cc723938103c0
dmytro.kuzmenko <dmytro.kuzmenko@bigcommerce.com>
2019-09-27 17:33:46 +0300

    made docker work

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -8,9 +8,5 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 
-const $ = require('jquery');
-window.$ = $;
-window.jQuery = $;
-
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
