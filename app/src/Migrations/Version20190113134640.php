337781fbdaf776a2fbc3e49ea9601bed9ad537bc
dmytro.kuzmenko <dmytro.kuzmenko@bigcommerce.com>
2019-11-05 15:48:15 +0200

    add Kyiv to migrations change main text

--- app/src/Migrations/Version20190113134640.php
+++ app/src/Migrations/Version20190113134640.php
@@ -15,6 +15,7 @@ final class Version20190113134640 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('INSERT INTO region (`name`) VALUES ("м. Київ")');
         $this->addSql('INSERT INTO region (`name`) VALUES ("Вінницька область")');
         $this->addSql('INSERT INTO region (`name`) VALUES ("Волинська область")');
         $this->addSql('INSERT INTO region (`name`) VALUES ("Дніпропетровська область")');
