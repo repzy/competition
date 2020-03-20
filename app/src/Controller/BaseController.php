0f5e1bd604f5464a8e6010ce246351d8e724fbdd
dmytro.kuzmenko <dmytro.kuzmenko@bigcommerce.com>
2020-03-20 18:22:26 +0200

    email

--- app/src/Controller/BaseController.php
+++ app/src/Controller/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Ramsey\Uuid\Uuid;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,5 +14,19 @@ class BaseController extends AbstractController
     public function indexAction()
     {
         return $this->redirect($this->generateUrl("competitions_list"));
+    }
+
+    /**
+     * @Route("/email")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function mailerAction()
+    {
+        return $this->render("emails/resetting.html.twig", [
+            'url' => $this->generateUrl('resetting_change', [
+                'code' => Uuid::uuid4()->toString()
+            ])
+        ]);
     }
 }
